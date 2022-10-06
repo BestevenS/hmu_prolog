ce60e10265cdf2ae87fb9f2fd966ce3cbeee9aa2
Besteven <stefanostei18@gmail.com>
2022-10-06 17:30:39 +0300

    Initial commit

---
+++ pdf1.pl
@@ -0,0 +1,9 @@
+antras(yannis, maria).
+pateras(yannis, kostas).
+mitera(maria, kostas).
+pateras(kostas, anna).
+pateras(kostas, aris).
+mitera(soula, anna).
+mitera(soula, aris).
+antras(kostas, soula).
+adelfi(anna, aris).
