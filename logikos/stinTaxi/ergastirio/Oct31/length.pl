1cad5bacbb59f5affa88186dad57ae355bdf0bf1
Besteven <stefanostei18@gmail.com>
2022-10-31 10:35:00 +0200

    length.pl file added it show how length works

---
+++ logikos/stinTaxi/ergastirio/Oct31/length.pl
@@ -0,0 +1,18 @@
+len(L, N):-
+    len1(L, 0, N).
+
+len1([], M, M).
+
+len1([_|T], M, N):-
+    M1 is M + 1,
+    len1(T, M1, N).
+
+
+/*
+
+len([], 0).
+len([_|T], N):-
+    len(T, N1),
+    N is N1 + 1.
+
+*/
