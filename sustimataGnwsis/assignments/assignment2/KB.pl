984a6e8eb25306d6dbda47f05db3955b4a997d8c
Besteven <stefanostei18@gmail.com>
2022-11-11 05:22:35 +0200

    Created the right KB.pl file

--- sustimataGnwsis/assignments/assignment2/KB.pl
+++ sustimataGnwsis/assignments/assignment2/KB.pl
@@ -1,25 +1,82 @@
-:- dynamic rules/1, max_ruleId/1, rule/3.
+rule(values(rid1, Koresmos, [Yparxi_NO3, Kokkina_oligochate, Yparxi_Izhmatwn, 
+Yparxi_H2S, Tupos_upostromatos, Yparxi_CH4, Murodia_Nerou]), Answer):-
+number(Koresmos),Koresmos >= 20,!,
+Answer = 'Kanena provlima anoxias'.
 
-rules([rid1, rid2, rid3, rid4, rid7]).
+rule(values(rid2, Koresmos, [Yparxi_NO3, Kokkina_oligochate, Yparxi_Izhmatwn, 
+Yparxi_H2S, Tupos_upostromatos, Yparxi_CH4, Murodia_Nerou]), Answer):-
+number(Koresmos),Koresmos < 20,
+Yparxi_NO3 == yes,!,
+Answer = 'Sovaro provlima anoxias'.
 
-max_ruleId(7).
+rule(values(rid3, Koresmos, [Yparxi_NO3, Kokkina_oligochate, Yparxi_Izhmatwn, 
+Yparxi_H2S, Tupos_upostromatos, Yparxi_CH4, Murodia_Nerou]), Answer):-
+number(Koresmos),Koresmos < 20,
+Yparxi_NO3 == no,!,
+Answer ='Poli sovaro provlima anoxias'.
 
-rule(rid1,values(yes, [BOD, Oxidability,DOC_TOC,[Cloudy,Colour]]),Answer) :- 
-    number(BOD), BOD =<7,
-    Answer='ΒΑΟ(BOD): Μέτριο πρόβλημα με ΜΟΥ.'.
+rule(values(rid4, Koresmos, [Yparxi_NO3, Kokkina_oligochate, Yparxi_Izhmatwn, 
+Yparxi_H2S, Tupos_upostromatos, Yparxi_CH4, Murodia_Nerou]), Answer):-
+Koresmos == no,
+Kokkina_oligochate == Arketa,!,
+Answer = 'Metria anoxias'.
 
-rule(rid2, values(yes, [BOD, Oxidability,DOC_TOC,[Cloudy,Colour]]),Answer) :-
-    number(BOD), BOD >7, BOD =< 15,
-    Answer='ΒΑΟ(BOD): Έντονο πρόβλημα ΜΟΥ.'.
+rule(values(rid5, Koresmos, [Yparxi_NO3, Kokkina_oligochate, Yparxi_Izhmatwn, 
+Yparxi_H2S, Tupos_upostromatos, Yparxi_CH4, Murodia_Nerou]), Answer):-
+Koresmos == no,write('yes'),
+Kokkina_oligochate == liga,!,
+Answer = 'Poli sovaro provlima anoxias'.
 
-rule(rid3, values(yes, [BOD, Oxidability,DOC_TOC,[Cloudy,Colour]]),Answer) :-
-    number(BOD),BOD >15,
-    Answer= 'ΒΑΟ(BOD): Πολύ Έντονο πρόβλημα ΜΟΥ.'.
+rule(values(rid6, Koresmos, [Yparxi_NO3, Kokkina_oligochate, Yparxi_Izhmatwn, 
+Yparxi_H2S, Tupos_upostromatos, Yparxi_CH4, Murodia_Nerou]), Answer):-
+Koresmos == no,
+Kokkina_oligochate == no,
+(Yparxi_Izhmatwn == maura; Yparxi_Izhmatwn == skoura),
+Yparxi_H2S == yes,!,
+Answer = 'Poli sovaro provlima anoxias'.
 
-rule(rid4, values(yes, [BOD, Oxidability,DOC_TOC,[Cloudy,Colour]]),Answer) :-
-    number(Oxidability), Oxidability =< 3,
-    Answer='Οξείδωση (Oxidability): Έντονο πρόβλημα ΜΟΥ.'.
+rule(values(rid7, Koresmos, [Yparxi_NO3, Kokkina_oligochate, Yparxi_Izhmatwn, 
+Yparxi_H2S, Tupos_upostromatos, Yparxi_CH4, Murodia_Nerou]), Answer):-
+Koresmos == no,
+Kokkina_oligochate == no,
+(Yparxi_Izhmatwn == maura; Yparxi_Izhmatwn == skoura),
+Yparxi_H2S == no,
+Yparxi_CH4 == yes,!,
+Answer = 'Poli savro provlima anoxias'.
 
-rule(rid7, values(yes, [BOD, Oxidability,DOC_TOC,[Cloudy,Colour]]),Answer) :-
-    number(Oxidability), Oxidability >3,
-    Oxidability =<7, Answer='Οξείδωση (Oxidability): Μέτριο πρόβλημα με ΜΟΥ.'. 
+rule(values(rid8, Koresmos, [Yparxi_NO3, Kokkina_oligochate, Yparxi_Izhmatwn, 
+Yparxi_H2S, Tupos_upostromatos, Yparxi_CH4, Murodia_Nerou]), Answer):-
+Koresmos == no,
+Kokkina_oligochate == no,
+(Yparxi_Izhmatwn == maura; Yparxi_Izhmatwn == skoura),
+Yparxi_H2S == no,
+Yparxi_CH4 == no,!,
+Answer = 'Sovaro provlima anoxias'.
+
+rule(values(rid9, Koresmos, [Yparxi_NO3, Kokkina_oligochate, Yparxi_Izhmatwn, 
+Yparxi_H2S, Tupos_upostromatos, Yparxi_CH4, Murodia_Nerou]), Answer):-
+Koresmos == no,
+Kokkina_oligochate == no,
+Yparxi_Izhmatwn == anoixta,
+(Tupos_upostromatos == mesaio; Tupos_upostromatos == megalo),!,
+Answer = 'Kanena provlima anoxias'.
+
+
+rule(values(rid10, Koresmos, [Yparxi_NO3, Kokkina_oligochate, Yparxi_Izhmatwn, 
+Yparxi_H2S, Tupos_upostromatos, Yparxi_CH4, Murodia_Nerou]), Answer):-
+Koresmos == no,
+Kokkina_oligochate == no,
+Yparxi_Izhmatwn == anoixta,
+Tupos_upostromatos == kanonikos,
+Murodia_Nerou == no,!,
+Answer = 'Kanena provlima anoxias'.
+
+
+rule(values(rid11, Koresmos, [Yparxi_NO3, Kokkina_oligochate, Yparxi_Izhmatwn, 
+Yparxi_H2S, Tupos_upostromatos, Yparxi_CH4, Murodia_Nerou]), Answer):-
+Koresmos == no,
+Kokkina_oligochate == no,
+Yparxi_Izhmatwn == anoixta,
+Tupos_upostromatos == kanonikos,
+Murodia_Nerou == yes,!,
+Answer = 'Metria anoxia'.
