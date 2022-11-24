7a9a8f083b759252ff43b81bfca4ca57926e0c46
Besteven <stefanostei18@gmail.com>
2022-11-24 03:09:18 +0200

    fix error and create the first hello world in browser

--- sustimataGnwsis/assignments/assignment3/prologhtml.pl
+++ sustimataGnwsis/assignments/assignment3/prologhtml.pl
@@ -1,19 +1,25 @@
+% to use the 
+
+:- use_module(library(http/thread_httpd)).
+:- use_module(library(http/http_dispatch)).
+
 % first argument (/) the URI that server has to serve
 % second argument (say_hi) the name of the function that will be called when the URI is requested
 % third argument ([]), the options for the function
-:- http_handler(./, say_hi, []). 
-% meaning of the line 4
+
+:- http_handler(/, say_hi, []).
+
+% meaning of the line ^
 % do the request at the root of the server with calling the function say_hi
 
 % just to be user friendly
-server(Port):-
-% :-
-%     write('Give the port number: '),
-%     read(Port),
+% server(Port):-
+:-
+    write('Give the port number: '),
+    read(Port),
     http_server(http_dispatch, [port(Port)]).
 
 % the function that will be called when the URI is requested
 say_hi(_Request):-
     format('Content-type: text/plain~n~n'), % must have header
     format('Hello World!~n'). % The output message
-    
