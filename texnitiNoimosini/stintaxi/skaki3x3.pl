b37cee5abef8482278cc17213f5f82cafadb1175
Besteven <stefanostei18@gmail.com>
2023-03-06 15:34:17 +0200

    The assignment is ready

--- texnitiNoimosini/stintaxi/skaki3x3.pl
+++ texnitiNoimosini/stintaxi/skaki3x3.pl
@@ -1,34 +1,50 @@
 % Kiniseis alogou se mia skakiera 3*3
 
-% goal
-% ?- PositionKnight=2, X2=1,monopati([[0,X2,0,0,0,0,0,0,0], PositionKnight], [[1, 1, 1, 1, 0, 1, 1, 1, 1], FinalPositionKnight],Path).
+% goals
+% ?- PositionKnight = 2, X2 = 1, monopati([[0, X2, 0, 0, 0, 0, 0, 0, 0], PositionKnight], [[1, 1, 1, 1, 0, 1, 1, 1, 1], FinalPositionKnight], Path).
+% ?- PositionKnight = 1, X1 = 1, monopati([[X1, 0, 0, 0, 0, 0, 0, 0, 0], PositionKnight], [[1, 1, 1, 1, 0, 1, 1, 1, 1], FinalPositionKnight], Path).
 
 % oles oi kiniseis pou mporoun na ginoun
-akme(a, c).
-akme(c, a).
-akme(c, e).
-akme(c, f).
-akme(b, c).
-akme(b, d).
-akme(d, f).
-akme(d, g).
+move(1, 8).
+move(1, 6).
+move(2, 9).
+move(2, 7).
+move(3, 4).
+move(3, 8).
+move(4, 9).
+move(4, 3).
+move(6, 1).
+move(6, 7).
+move(7, 2).
+move(7, 6).
+move(8, 3).
+move(8, 1).
+move(9, 2).
+move(9, 4).
 
-telike_katastase([[1, 1, 1, 1, 0, 1, 1, 1, 1]]).
 
-% dinoume monopati(teliko-monopati[[1, 1, 1, 1, 0, 1, 1, 1, 1], [teliki_thesi]], Xrisimopoieitai san Bima parakatw, Monopati[tha ftiaxtei mono toy])
-monopati(X, Y, Monopati):-
-    monopati1(X, Y, [X], Monopati).
+efarmogi_telesti([State, PosKnight], [NewState, NewPosKnight]):-
+    move(PosKnight, NewPosKnight),
+    validMove_updateChess(State, NewPosKnight, NewState).
 
-% an ftasei teliki katastasi na teleiwsei
-monopati1(X, X, Monopati, Monopati) :- 
-    telike_katastase(X).
+validMove_updateChess([H1|T1], 1, [1|T1]):-
+    H1==0.
 
-monopati1(X, Z, Monopati, Teliko_monopati) :-
-    % gia X oti akme uparxei tha tin parei i metabliti Y
-    akme(X, Y),
-    % an den uparxei to Y sto Monopati
-    \+ member(Y, Monopati),
-    % tote tha prosthesoume to Y sto Monopati
-    append(Monopati, [Y], Neo_monopati),
-    % anadromika 
-    monopati1(Y, Z, Neo_monopati, Teliko_monopati).
+validMove_updateChess([H1|T1], Counter, [H1|T2]) :-
+    Counter > 1,
+    NewCounter is Counter -1,
+    validMove_updateChess(T1, NewCounter, T2).
+
+
+monopati([X, InitPosKnight], [Y, FinalPosKnight], Monopati):-
+    monopati1([X, InitPosKnight], [Y, FinalPosKnight], [InitPosKnight], Monopati).
+
+monopati1([X, PosKnight], [Y, FinalPosKnight], Monopati, Monopati):-
+    X == Y,
+    PosKnight = FinalPosKnight.
+
+monopati1([X, PosKnight], [Z, FinalPosKnight], Monopati, Teliko_monopati):-
+    efarmogi_telesti([X, PosKnight], [Y, NewPosKnight]),
+    \+ member(NewPosKnight, Monopati),
+    append(Monopati, [NewPosKnight], Neo_monopati),
+    monopati1([Y, NewPosKnight], [Z, FinalPosKnight], Neo_monopati, Teliko_monopati).
