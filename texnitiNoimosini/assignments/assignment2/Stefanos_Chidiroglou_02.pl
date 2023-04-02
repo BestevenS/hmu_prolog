edc598775cf8ff2891ce8b0d29afaf4c6f0efbfc
Besteven <stefanostei18@gmail.com>
2023-04-02 18:49:12 +0300

    Plagentis solution clean

--- texnitiNoimosini/assignments/assignment2/Stefanos_Chidiroglou_02.pl
+++ texnitiNoimosini/assignments/assignment2/Stefanos_Chidiroglou_02.pl
@@ -42,14 +42,14 @@ dequeue(X, [X|T], T).
 enqueue(X, [], [X]).
 enqueue(X, [H|T1], [H|T2]):- 
 	X \= H,
-	enqueue(X,T1,T2).
+	enqueue(X, T1, T2).
 						
 add_list_to_queue1(RestChildren, NQ, FQ).
 add_list_to_queue(Child, Q1, NQ):-
-	add_list_to_queue1(Child,Q1,NQ).
+	add_list_to_queue1(Child, Q1, NQ).
 
 add_list_to_queue1([], FQ, FQ).
-add_list_to_queue1([Child|RestChildren], Q1, FQ):- 
+add_list_to_queue1([Child|RestChildren], Q1, FQ):-
 	append(Q1, [Child], NQ),
 	add_list_to_queue1(RestChildren, NQ, FQ).
 
@@ -59,23 +59,27 @@ print_path([Kat, met], _):-
 print_path([Kat, Par], S1):-
 	member_set([Par, Par2], S1),
 	print_path([Par, Par2], S1),
-	write('--> '), 
-	write(Kat) . 
+	write('--> '),
+	write(Kat).
 
-get_children(Kat,Rest, S1,Children) :- bagof(Child, moves(Kat, Rest, S1, Child), Children).
-
+get_children(Kat, Rest, S1, Children):-
+	bagof(Child, moves(Kat, Rest, S1, Child), Children).
+	
 get_children(Kat,Rest,Closed_set,[]).
-moves(Kat,Rest,S1, [Next,Kat]) :- move(Kat, Next), \+(member([Next,Node], Rest)),  \+(member_set([Next,_NodeY], S1)). 	  
+moves(Kat,Rest,S1, [Next,Kat]):-
+	move(Kat, Next),
+	\+(member([Next,Node], Rest)),
+	\+(member_set([Next,_NodeY], S1)).
 												  
 % BFS
 
-breadthFirstSearch(Arxi, Goal) :- 
+breadthFirstSearch(Arxi, Goal):-
 	empty_queue(EmptyQueue),
 	enqueue([Arxi, met], EmptyQueue, Q2),
 	empty_set(S1),
 	path(Q2, S1, Goal).
 
-path(Q2, _, _) :-
+path(Q2, _, _):-
 	empty_queue(Q2),
 	write('xoris lisi').
 
@@ -86,7 +90,7 @@ path(Q2, S1, Goal):-
 	write('monopati lisis:'), nl,
 	print_path([Kat, Par], S1).
 
-path(Q2, S1, Goal) :- 
+path(Q2, S1, Goal):-
 	dequeue([Kat, Par], Q2, Rest), 
 	get_children(Kat, Rest,S1,Children),
 	add_list_to_queue(Children, Rest, NQ2),
